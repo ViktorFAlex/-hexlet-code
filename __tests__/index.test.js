15a055e5cfb9bad275d2007ebc9ada0267e8abaf
ViktorFAlex <viktor.f.alex@gmail.com>
2022-09-07 20:14:52 +0800

    refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,21 +11,21 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 
 const files = [['file1.json', 'file2.json'], ['file1.yaml', 'file2.yaml'], ['file1.json', 'file2.yaml']];
 
-test.each(files)('description', (file1, file2) => {
+test.each(files)('gendiff for "stylish" format', (file1, file2) => {
   const file1path = getFixturePath(file1);
   const file2path = getFixturePath(file2);
   const result = readFile('resultStylish.txt');
   expect(genDiff(file1path, file2path, 'stylish')).toEqual(result);
 });
 
-test.each(files)('description', (file1, file2) => {
+test.each(files)('gendiff for "plain" format', (file1, file2) => {
   const file1path = getFixturePath(file1);
   const file2path = getFixturePath(file2);
   const result = readFile('resultPlain.txt');
   expect(genDiff(file1path, file2path, 'plain')).toEqual(result);
 });
 
-test.each(files)('description', (file1, file2) => {
+test.each(files)('gendiff for "json" format', (file1, file2) => {
   const file1path = getFixturePath(file1);
   const file2path = getFixturePath(file2);
   const result = readFile('resultJson.txt');
--- src/index.js
+++ src/index.js
@@ -8,43 +8,87 @@ const mergeKeys = (data1, data2) => {
   return _.union(keys1, keys2);
 };
 
+const getState = (value1, value2) => {
+  switch (true) {
+    case (!_.isObject(value1) && !_.isObject(value2)):
+      switch (true) {
+        case (value1 === value2):
+          return 1;
+        case (value1 !== undefined && value2 !== undefined):
+          return 2;
+        case (value1 === undefined):
+          return 3;
+        default:
+          return 4;
+      }
+    case (_.isObject(value1) && _.isObject(value2)):
+      return 5;
+    case (_.isObject(value1) && value2 !== undefined):
+      return 6;
+    case (_.isObject(value2) && value1 !== undefined):
+      return 7;
+    case (value1 === undefined):
+      return 8;
+    case (value2 === undefined):
+      return 9;
+    default:
+      throw new Error('Unexpectated state of values');
+  }
+};
+
+const addNewTypes = (state, elem, isChangeKey) => {
+  const typeForDeleted = isChangeKey ? 'deleted' : 'unchanged';
+  const typeForAdded = isChangeKey ? 'added' : 'unchanged';
+  const obj = { name: elem };
+  switch (state) {
+    case (1):
+    case (5):
+      return [{ ...obj, type: 'unchanged' }, {}];
+    case (2):
+    case (6):
+    case (7):
+      return [{ ...obj, type: 'changed' }, { ...obj, type: 'set' }];
+    case (3):
+    case (8):
+      return [{ ...obj, type: `${typeForAdded}` }, {}];
+    default:
+      return [{ ...obj, type: `${typeForDeleted}` }, {}];
+  }
+};
+
+const getChildrenValues = (node, data1, data2) => [_.get(data1, node), _.get(data2, node)];
+
+const iter = (data1, data2, isChangeType = true) => {
+  const sortedKeys = _.sortBy(mergeKeys(data1, data2));
+  return sortedKeys.flatMap((elem) => {
+    const [val1, val2] = getChildrenValues(elem, data1, data2);
+    const state = getState(val1, val2);
+    const [obj1, obj2] = addNewTypes(state, elem, isChangeType);
+    switch (state) {
+      case (1):
+      case (4):
+        return [{ ...obj1, children: val1 }];
+      case (2):
+        return [{ ...obj1, children: val1 }, { ...obj2, children: val2 }];
+      case (3):
+        return [{ ...obj1, children: val2 }];
+      case (5):
+        return [{ ...obj1, children: iter(val1, val2) }];
+      case (6):
+        return [{ ...obj1, children: iter(val1, {}, false) }, { ...obj2, children: val2 }];
+      case (7):
+        return [{ ...obj1, children: val1 }, { ...obj2, children: iter({}, val2, false) }];
+      case (8):
+        return [{ ...obj1, children: iter(val2, {}, false) }];
+      default:
+        return [{ ...obj1, children: iter({}, val1, false) }];
+    }
+  });
+};
+
 export default (path1, path2, formatName) => {
   const dataObject1 = parse(path1);
   const dataObject2 = parse(path2);
-  const iter = (data1, data2, changeKey = true) => {
-    const sortedKeys = _.sortBy(mergeKeys(data1, data2));
-    // don't change type if comparing nested elems;
-    const typeForDeleted = changeKey ? 'deleted' : 'unchanged';
-    const typeForAdded = changeKey ? 'added' : 'unchanged';
-    return sortedKeys.reduce((acc, elem) => {
-      const children1 = _.get(data1, elem);
-      const children2 = _.get(data2, elem);
-      if (!_.isObject(children1) && !_.isObject(children2)) {
-        if (children1 === children2) {
-          return [...acc, { name: elem, type: 'unchanged', children: children1 }];
-        }
-        if (children1 !== undefined && children2 !== undefined) {
-          return [...acc, { name: elem, type: 'changed', children: children1 },
-            { name: elem, type: 'set', children: children2 }];
-        }
-        return children1 === undefined ? [...acc, { name: elem, type: `${typeForAdded}`, children: children2 }]
-          : [...acc, { name: elem, type: `${typeForDeleted}`, children: children1 }];
-      }
-      if (_.isObject(children1) && _.isObject(children2)) {
-        return [...acc, { name: elem, type: 'unchanged', children: iter(children1, children2) }];
-      }
-      if (_.isObject(children1) && children2 !== undefined) {
-        return [...acc, { name: elem, type: 'changed', children: iter(children1, {}, false) },
-          { name: elem, type: 'set', children: children2 }];
-      }
-      if (_.isObject(children2) && children1 !== undefined) {
-        return [...acc, { name: elem, type: 'changed', children: children1 },
-          { name: elem, type: 'set', children: iter({}, children2, false) }];
-      }
-      return children1 ? [...acc, { name: elem, type: `${typeForDeleted}`, children: iter(children1, {}, false) }]
-        : [...acc, { name: elem, type: `${typeForAdded}`, children: iter({}, children2, false) }];
-    }, []);
-  };
   const result = iter(dataObject1, dataObject2, true);
   return format(result, formatName);
 };
