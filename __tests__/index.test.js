281378d496c625e3bc72c8ad1cf51fed49f4716d
ViktorFAlex <viktor.f.alex@gmail.com>
2022-09-12 18:32:22 +0800

    add yml tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,7 +9,8 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const files = [['file1.json', 'file2.json'], ['file1.yaml', 'file2.yaml'], ['file1.json', 'file2.yaml']];
+const files = [['file1.json', 'file2.json'], ['file1.yaml', 'file2.yml'],
+  ['file1.json', 'file2.yml'], ['file1.yaml', 'file2.json']];
 
 test.each(files)('gendiff for "stylish" format', (file1, file2) => {
   const file1path = getFixturePath(file1);
