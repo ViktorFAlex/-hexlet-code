82bf476832bc0119cbf0b61edac4059317fa261c
ViktorFAlex <viktor.f.alex@gmail.com>
2022-09-16 13:19:10 +0800

    change index.js

--- src/index.js
+++ src/index.js
@@ -15,24 +15,26 @@ const buildTree = (obj1, obj2) => {
   const sortedKeys = _.sortBy(mergeKeys(obj1, obj2));
   return sortedKeys.flatMap((key) => {
     const [val1, val2] = getChildrenValues(key, obj1, obj2);
-    if (_.isObject(val1) && _.isObject(val2)) {
-      return { key, type: 'nested', children: buildTree(val1, val2) };
+    switch (true) {
+      case (_.isObject(val1) && _.isObject(val2)): 
+        return { key, type: 'nested', children: buildTree(val1, val2) };
+      case (val1 === val2):
+        return { key, type: 'unchanged', children: val1 };
+      case (_.has(obj1, key) && _.has(obj2, key)): {
+        const children1 = _.isObject(val1) ? buildTree(val1) : val1;
+        const children2 = _.isObject(val2) ? buildTree(val2) : val2;
+        return { key, type: 'changed', children: { old: children1, new: children2 } };
+      }
+      default: {
+        const children = val1 || val2;
+        const newChildren = _.isObject(children) ? buildTree(children) : children;
+        if (!obj1 || !obj2) {
+          return { key, type: 'incomparable', children: newChildren };
+        }
+        const newType = val1 ? 'deleted' : 'added';
+        return { key, type: newType, children: newChildren };
+      }
     }
-    if (val1 === val2) {
-      return { key, type: 'unchanged', children: val1 };
-    }
-    if (_.has(obj1, key) && _.has(obj2, key)) {
-      const children1 = _.isObject(val1) ? buildTree(val1) : val1;
-      const children2 = _.isObject(val2) ? buildTree(val2) : val2;
-      return { key, type: 'changed', children: { old: children1, new: children2 } };
-    }
-    const children = val1 || val2;
-    const newChildren = _.isObject(children) ? buildTree(children) : children;
-    if (!obj1 || !obj2) {
-      return { key, type: 'incomparable', children: newChildren };
-    }
-    const newType = val1 ? 'deleted' : 'added';
-    return { key, type: newType, children: newChildren };
   });
 };
 
