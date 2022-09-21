cea903160b87835b9a7ecfd9c2570f48ace97f06
ViktorFAlex <viktor.f.alex@gmail.com>
2022-09-21 21:00:22 +0800

    change climate

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -14,32 +14,33 @@ const generateIndents = (depth) => {
   return [linesIndent, bracketIndent];
 };
 
-const format = (node, depth = 0) => {
-  if (!Array.isArray(node)) {
-    return _.isObject(node) ? format(Object.entries(node), depth) : node;
-  }
-  const [linesIndent, bracketIndent] = generateIndents(depth);
-  const result = node.flatMap((elem) => {
-    if (Array.isArray(elem)) {
-      const [key, value] = elem;
-      return `${linesIndent}${symbols.doubleSpace}${key}: ${format(value, depth + 1)}`;
+export default (obj) => {
+  const format = (node, depth = 0) => {
+    if (!Array.isArray(node)) {
+      return _.isObject(node) ? format(Object.entries(node), depth) : node;
     }
-    const { key, type, children } = elem;
-    if (type === 'changed') {
-      const { old: oldVal, new: newVal } = children;
-      const firstSymbol = symbols.minusWithSpace;
-      const secondSymbol = symbols.plusWithSpace;
-      const firstNode = `${linesIndent}${firstSymbol}${key}: ${format(oldVal, depth + 1)}`;
-      const secondNode = `${linesIndent}${secondSymbol}${key}: ${format(newVal, depth + 1)}`;
-      return [firstNode, secondNode];
-    }
-    if (type === 'unchanged' || type === 'nested') {
-      return `${linesIndent}${symbols.doubleSpace}${key}: ${format(children, depth + 1)}`;
-    }
-    const newSymbol = type === 'added' ? symbols.plusWithSpace : symbols.minusWithSpace;
-    return `${linesIndent}${newSymbol}${key}: ${format(children, depth + 1)}`;
-  });
-  return ['{', ...result, `${bracketIndent}}`].join('\n');
+    const [linesIndent, bracketIndent] = generateIndents(depth);
+    const result = node.flatMap((elem) => {
+      if (Array.isArray(elem)) {
+        const [key, value] = elem;
+        return `${linesIndent}${symbols.doubleSpace}${key}: ${format(value, depth + 1)}`;
+      }
+      const { key, type, children } = elem;
+      if (type === 'changed') {
+        const { old: oldVal, new: newVal } = children;
+        const firstSymbol = symbols.minusWithSpace;
+        const secondSymbol = symbols.plusWithSpace;
+        const firstNode = `${linesIndent}${firstSymbol}${key}: ${format(oldVal, depth + 1)}`;
+        const secondNode = `${linesIndent}${secondSymbol}${key}: ${format(newVal, depth + 1)}`;
+        return [firstNode, secondNode];
+      }
+      if (type === 'unchanged' || type === 'nested') {
+        return `${linesIndent}${symbols.doubleSpace}${key}: ${format(children, depth + 1)}`;
+      }
+      const newSymbol = type === 'added' ? symbols.plusWithSpace : symbols.minusWithSpace;
+      return `${linesIndent}${newSymbol}${key}: ${format(children, depth + 1)}`;
+    });
+    return ['{', ...result, `${bracketIndent}}`].join('\n');
+  };
+  return format(obj);
 };
-
-export default format;
